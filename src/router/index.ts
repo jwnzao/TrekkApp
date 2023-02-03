9758b6f8fff61cf083c041ca23bfe68f862d8741
= <jeff.aprentiv@gmail.com>
2023-02-03 09:19:46 +0100

    The project application created

---
+++ src/router/index.ts
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import TabsPage from '../views/TabsPage.vue'
+import MenuPage from '../views/MenuPage.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/menu'
+  },
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/tab1'
+      },
+      {
+        path: 'tab1',
+        component: () => import('@/views/Tab1Page.vue')
+      },
+      {
+        path: 'tab2',
+        component: () => import('@/views/Tab2Page.vue')
+      },
+      {
+        path: 'tab3',
+        component: () => import('@/views/Tab3Page.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,65 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ExploreContainer name="Tab 2 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+  import { camera, trash, close } from 'ionicons/icons';
+  import {
+    IonPage,
+    IonHeader,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  } from '@ionic/vue';
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  import ExploreContainer from '@/components/ExploreContainer.vue';
+
+  export default {
+    name: 'Tab2',
+    components: {
+      IonPage,
+      IonHeader,
+      IonFab,
+      IonFabButton,
+      IonIcon,
+      IonToolbar,
+      IonTitle,
+      IonContent,
+      IonGrid,
+      IonRow,
+      IonCol,
+      IonImg,
+    },
+    setup() {
+      return {
+        camera,
+        trash,
+        close,
+      };
+    },
+  };
+</script>
+
+
